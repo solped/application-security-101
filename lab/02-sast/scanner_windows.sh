00be07ea8a7222af2b4491f01b6905866cbd4942
worasit <worasit.daimongkol@thoughtworks.com>
2023-11-23 21:39:16 +0700

    updated script for Windows laptop

--- lab/02-sast/scanner_windows.sh
+++ lab/02-sast/scanner_windows.sh
@@ -1,13 +1,1 @@
-export SONARQUBE_URL="localhost:9000"
-export YOUR_PROJECT_KEY="sast-demo"
-export AUTH_TOKEN="sqp_38734db8c91d2223e36e11afd320c03fb9192900"
-
-
-docker run \
-    --network=host \
-    --rm \
-    -e SONAR_HOST_URL="http://${SONARQUBE_URL}" \
-    -e SONAR_SCANNER_OPTS="-Dsonar.projectKey=${YOUR_PROJECT_KEY}" \
-    -e SONAR_TOKEN="${AUTH_TOKEN}" \
-    -v "${PWD}:/usr/src" \
-    sonarsource/sonar-scanner-cli
+docker run --network=host --rm -e SONAR_HOST_URL="http://localhost:9000" -e SONAR_SCANNER_OPTS="-Dsonar.projectKey=AppSec101" -e SONAR_TOKEN="sqp_eae9613f3ad361fb1267a77593e5893c7cf4bd0f" -v "${PWD}:/usr/src" sonarsource/sonar-scanner-cli
